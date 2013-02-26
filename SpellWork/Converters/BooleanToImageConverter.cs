95b3776a385c94e1c0cb71fa24769f8ebffa2819
Konctantin <gawrilyako@gmail.com>
2013-02-26 18:56:17 +0400

    ...
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

---
+++ SpellWork/Converters/BooleanToImageConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace SpellWork.Converters
+{
+    public class BooleanToImageConverter : ConverterBase<BooleanToImageConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //todo: return (bool)value 
+            //          ? new Uri("pack://application:,,,/SpellWork;component/Resources/Plus.png") 
+            //          : new Uri("pack://application:,,,/SpellWork;component/Resources/Minus.png");
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ SpellWork/Converters/ConverterBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace SpellWork.Converters
+{
+    public abstract class ConverterBase<T> 
+        : MarkupExtension, IValueConverter where T : class, new()
+    {
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (m_converter == null)
+                m_converter = new T();
+            return m_converter;
+        }
+
+        private static T m_converter = null;
+    }
+}
