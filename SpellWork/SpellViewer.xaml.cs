c213a897992fb175bbd5f877b669256d1e515a30
Konctantin <gawrilyako@gmail.com>
2013-02-28 00:07:02 +0400

    ...
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- SpellWork/SpellViewer.xaml.cs
+++ SpellWork/SpellViewer.xaml.cs
@@ -57,58 +57,33 @@ namespace SpellWork
                 return;
             }
 
-            var spellids = new List<uint>();
+            var spellids = (from spell in DBC.Spell.Values
 
-            // speedup variant
-            if (spellFamily != -1 || spellEffect != -1 || auraType != -1 || targetA != -1 || targetB != -1)
-            {
-                spellids = (from spell in DBC.Spell.Values
+                        join spe in DBC.SpellEffect.Records on spell.ID equals spe.EffectSpellId into _spe
+                        from spellEffectEntry in _spe.DefaultIfEmpty()
 
-                            join spe in DBC.SpellEffect.Records on spell.ID equals spe.EffectSpellId into _spe
-                            from spellEffectEntry in _spe.DefaultIfEmpty()
+                        join sco in DBC.SpellClassOptions.Records on spell.SpellClassOptionsId equals sco.Id into _sco
+                        from spellClassOptionEntry in _sco.DefaultIfEmpty()
 
-                            join sco in DBC.SpellClassOptions.Records on spell.SpellClassOptionsId equals sco.Id into _sco
-                            from spellClassOptionEntry in _sco.DefaultIfEmpty()
+                        where (spellId == 0 || spell.ID == spellId)
+                            && (string.IsNullOrWhiteSpace(tbSpellName.Text) || spell.SpellName.IndexOf(tbSpellName.Text, StringComparison.CurrentCultureIgnoreCase) > -1)
 
-                            where (spellFamily == -1 || (spellClassOptionEntry != null && spellClassOptionEntry.SpellFamilyName == spellFamily))
+                            && (spellFamily == -1 || (spellClassOptionEntry != null && spellClassOptionEntry.SpellFamilyName == spellFamily))
 
-                               && (spellEffect == -1 || (spellEffectEntry != null && spellEffectEntry.Effect == spellEffect))
-                               && (auraType    == -1 || (spellEffectEntry != null && spellEffectEntry.EffectApplyAuraName == auraType))
-                               && (targetA     == -1 || (spellEffectEntry != null && spellEffectEntry.EffectImplicitTargetA == targetA))
-                               && (targetB     == -1 || (spellEffectEntry != null && spellEffectEntry.EffectImplicitTargetB == targetB))
-                            #warning todo : implement additional filter
-                               //&& (!use1val      || spell.CreateFilter(field1, advVal1, field1ct))
-                               //&& (!use2val      || spell.CreateFilter(field2, advVal2, field2ct))
-                           select spell.ID).ToList();
-            }
-
-            var hasAdditionalFilter = spellids.Count() > 0;
+                            && (spellEffect == -1 || (spellEffectEntry != null && spellEffectEntry.Effect == spellEffect))
+                            && (auraType    == -1 || (spellEffectEntry != null && spellEffectEntry.EffectApplyAuraName == auraType))
+                            && (targetA     == -1 || (spellEffectEntry != null && spellEffectEntry.EffectImplicitTargetA == targetA))
+                            && (targetB     == -1 || (spellEffectEntry != null && spellEffectEntry.EffectImplicitTargetB == targetB))
+                            
+                        #warning todo : implement additional filter
+                            //&& (!use1val      || spell.CreateFilter(field1, advVal1, field1ct))
+                            //&& (!use2val      || spell.CreateFilter(field2, advVal2, field2ct))
+                        select spell.ID).ToList();
 
             if (spellList != null)
-            {
-                spellList.Filter = new Predicate<object>((spellRaw) =>
-                {
-                    if (!(spellRaw is SpellEntry))
-                        return false;
-
-                    var spell = spellRaw as SpellEntry;                    
-
-                    // SpellName of Id
-                    if (!string.IsNullOrWhiteSpace(tbSpellName.Text))
-                    {
-                        if (spellId > 0 && spell.ID == spellId)
-                            return true;
-
-                        if (spell.SpellName.IndexOf(tbSpellName.Text, StringComparison.CurrentCultureIgnoreCase) > -1)
-                            return true;
-                    }
-
-                    if (hasAdditionalFilter && spellids.Contains(spell.ID))
-                        return true;
-
-                    return false;
+                spellList.Filter = new Predicate<object>((spellRaw) => {
+                    return spellids.Contains((spellRaw as SpellEntry).ID);
                 });
-            }
         }
 
         private void filter_SelectionChanged(object sender, SelectionChangedEventArgs e)
