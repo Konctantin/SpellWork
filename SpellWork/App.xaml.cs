8f3cc33cfa2e52fd32509d4cbb78f7fd71deb97f
Konctantin <gawrilyako@gmail.com>
2015-11-04 21:56:54 +0300

    Move DB files load code
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- SpellWork/App.xaml.cs
+++ SpellWork/App.xaml.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using DBFilesClient.NET;
-using SpellWork.Dbc;
+using SpellWork.DBClientFilesStructure;
 
 namespace SpellWork
 {
@@ -26,7 +26,7 @@ namespace SpellWork
             {
                 //var sw = new Stopwatch();
                 //sw.Start();
-                LoadSpells();
+                DBC.Load();
                 //sw.Stop();
                 //MessageBox.Show(sw.ElapsedMilliseconds.ToString());
             }
@@ -44,89 +44,6 @@ namespace SpellWork
             }
 
             base.OnStartup(e);
-        }
-
-        DBCStorage<T> LoadDBC<T>() where T : class, new()
-        {
-            var name = typeof(T).Name;
-            name = name.Substring(0, name.Length - "Entry".Length);
-            var fileName = Path.Combine(DBC.DBC_PATH, name + ".dbc");
-
-            if (!File.Exists(fileName))
-                throw new FileNotFoundException(fileName);
-
-            var bytes = File.ReadAllBytes(fileName);
-            using (var ms = new MemoryStream(bytes))
-            {
-                try
-                {
-                    var tmp = new DBCStorage<T>();
-                    tmp.Load(ms);
-                    return tmp;
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception("Can't load " + fileName, ex);
-                }
-            }
-        }
-
-        DB2Storage<T> LoadDB2<T>() where T : class, new()
-        {
-            var name = typeof(T).Name;
-            name = name.Substring(0, name.Length - "Entry".Length);
-            var fileName = Path.Combine(DBC.DBC_PATH, name + ".db2");
-
-            if (!File.Exists(fileName))
-                throw new FileNotFoundException(fileName);
-
-            var bytes = File.ReadAllBytes(fileName);
-            using (var ms = new MemoryStream(bytes))
-            {
-                try
-                {
-                    var tmp = new DB2Storage<T>();
-                    tmp.Load(ms);
-                    return tmp;
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception("Can't load " + fileName, ex);
-                }
-            }
-        }
-
-        public void LoadSpells()
-        {
-            DBC.Spell                           = LoadDBC<SpellEntry>();
-            DBC.SpellMisc                       = LoadDB2<SpellMiscEntry>();
-            DBC.SpellEffect                     = LoadDBC<SpellEffectEntry>();
-            DBC.SpellTargetRestrictions         = LoadDBC<SpellTargetRestrictionsEntry>();
-            DBC.SpellAuraRestrictions           = LoadDB2<SpellAuraRestrictionsEntry>();
-            DBC.SpellCooldowns                  = LoadDBC<SpellCooldownsEntry>();
-            DBC.SpellCategories                 = LoadDBC<SpellCategoriesEntry>();
-            DBC.SpellShapeshift                 = LoadDBC<SpellShapeshiftEntry>();
-            DBC.SpellAuraOptions                = LoadDBC<SpellAuraOptionsEntry>();
-            DBC.SpellLevels                     = LoadDBC<SpellLevelsEntry>();
-            DBC.SpellClassOptions               = LoadDB2<SpellClassOptionsEntry>();
-            DBC.SpellCastingRequirements        = LoadDB2<SpellCastingRequirementsEntry>();
-            DBC.SpellPower                      = LoadDB2<SpellPowerEntry>();
-            DBC.SpellInterrupts                 = LoadDBC<SpellInterruptsEntry>();
-           // DBC.SpellEquippedItems              = LoadDBC<SpellEquippedItemsEntry>();
-           // DBC.SpellDuration                   = LoadDBC<SpellDurationEntry>();
-            DBC.SkillLineAbility                = LoadDBC<SkillLineAbilityEntry>();
-          //  DBC.SpellRadius                     = LoadDBC<SpellRadiusEntry>();
-          //  DBC.SpellCastTimes                  = LoadDBC<SpellCastTimesEntry>();
-          //  DBC.SpellDifficulty                 = LoadDBC<SpellDifficultyEntry>();
-          //  DBC.CurrencyTypes                   = LoadDBC<CurrencyTypesEntry>();
-          //  DBC.OverrideSpellData               = LoadDBC<OverrideSpellDataEntry>();
-            DBC.SkillLine                       = LoadDBC<SkillLineEntry>();
-         //   DBC.SpellRange                      = LoadDBC<SpellRangeEntry>();
-            DBC.SpellReagents                   = LoadDB2<SpellReagentsEntry>();
-         //   DBC.ScreenEffect                    = LoadDBC<ScreenEffectEntry>();
-         //   DBC.ItemClass                       = LoadDBC<ItemClassEntry>();
-            DBC.SpellShapeshiftForm             = LoadDBC<SpellShapeshiftFormEntry>();
-            //   DBC.SpellDispelType                 = LoadDBC<SpellDispelTypeEntry>();
         }
     }
 }
--- SpellWork/DbcStores.cs
+++ SpellWork/DbcStores.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using DBFilesClient.NET;
-using SpellWork.Dbc;
+using SpellWork.DBClientFilesStructure;
+using System.IO;
+using System;
 
 namespace SpellWork
 {
@@ -9,34 +11,93 @@ namespace SpellWork
         public const string VERSION = "SpellWork 6.2.2 (20574)";
         public const string DBC_PATH = "dbc";
 
-        //public static DBCStorage<CurrencyTypesEntry>            CurrencyTypes            { get; set; } = new DBCStorage<CurrencyTypesEntry>();
-        public static DBCStorage<SpellEntry>                    Spell                    { get; set; } = new DBCStorage<SpellEntry>();
-        public static DB2Storage<SpellMiscEntry>                SpellMisc                { get; set; } = new DB2Storage<SpellMiscEntry>();
-        public static DBCStorage<SpellEffectEntry>              SpellEffect              { get; set; } = new DBCStorage<SpellEffectEntry>();
-        public static DBCStorage<SpellTargetRestrictionsEntry>  SpellTargetRestrictions  { get; set; } = new DBCStorage<SpellTargetRestrictionsEntry>();
-        public static DB2Storage<SpellAuraRestrictionsEntry>    SpellAuraRestrictions    { get; set; } = new DB2Storage<SpellAuraRestrictionsEntry>();
-        public static DBCStorage<SpellCooldownsEntry>           SpellCooldowns           { get; set; } = new DBCStorage<SpellCooldownsEntry>();
-        public static DBCStorage<SpellCategoriesEntry>          SpellCategories          { get; set; } = new DBCStorage<SpellCategoriesEntry>();
-        public static DBCStorage<SpellShapeshiftEntry>          SpellShapeshift          { get; set; } = new DBCStorage<SpellShapeshiftEntry>();
-        public static DBCStorage<SpellAuraOptionsEntry>         SpellAuraOptions         { get; set; } = new DBCStorage<SpellAuraOptionsEntry>();
-        public static DBCStorage<SpellLevelsEntry>              SpellLevels              { get; set; } = new DBCStorage<SpellLevelsEntry>();
-        public static DB2Storage<SpellClassOptionsEntry>        SpellClassOptions        { get; set; } = new DB2Storage<SpellClassOptionsEntry>();
-        public static DB2Storage<SpellCastingRequirementsEntry> SpellCastingRequirements { get; set; } = new DB2Storage<SpellCastingRequirementsEntry>();
-        public static DB2Storage<SpellPowerEntry>               SpellPower               { get; set; } = new DB2Storage<SpellPowerEntry>();
-        public static DBCStorage<SpellInterruptsEntry>          SpellInterrupts          { get; set; } = new DBCStorage<SpellInterruptsEntry>();
-        //public static DBCStorage<SpellEquippedItemsEntry>       SpellEquippedItems       { get; set; } = new DBCStorage<SpellEquippedItemsEntry>();
-        //public static DBCStorage<SpellRadiusEntry>              SpellRadius              { get; set; } = new DBCStorage<SpellRadiusEntry>();
-        //public static DBCStorage<SpellCastTimesEntry>           SpellCastTimes           { get; set; } = new DBCStorage<SpellCastTimesEntry>();
-        //public static DBCStorage<SpellDifficultyEntry>          SpellDifficulty          { get; set; } = new DBCStorage<SpellDifficultyEntry>();
-        //public static DBCStorage<SpellRangeEntry>               SpellRange               { get; set; } = new DBCStorage<SpellRangeEntry>();
-        public static DB2Storage<SpellReagentsEntry>            SpellReagents            { get; set; } = new DB2Storage<SpellReagentsEntry>();
-        //public static DBCStorage<SpellDurationEntry>            SpellDuration            { get; set; } = new DBCStorage<SpellDurationEntry>();
-        public static DBCStorage<SkillLineAbilityEntry>         SkillLineAbility         { get; set; } = new DBCStorage<SkillLineAbilityEntry>();
-        public static DBCStorage<SkillLineEntry>                SkillLine                { get; set; } = new DBCStorage<SkillLineEntry>();
-        //public static DBCStorage<ScreenEffectEntry>             ScreenEffect             { get; set; } = new DBCStorage<ScreenEffectEntry>();
-        //public static DBCStorage<OverrideSpellDataEntry>        OverrideSpellData        { get; set; } = new DBCStorage<OverrideSpellDataEntry>();
-        //public static DBCStorage<ItemClassEntry>                ItemClass                { get; set; } = new DBCStorage<ItemClassEntry>();
-        public static DBCStorage<SpellShapeshiftFormEntry>      SpellShapeshiftForm      { get; set; } = new DBCStorage<SpellShapeshiftFormEntry>();
-        //public static DBCStorage<SpellDispelTypeEntry>          SpellDispelType          { get; set; } = new DBCStorage<SpellDispelTypeEntry>();
+        //public static DBCStorage<CurrencyTypesEntry>            CurrencyTypes          { get; private set; } = LoadDBC<CurrencyTypesEntry>();
+        public static DBCStorage<SpellEntry> Spell                                       { get; private set; } = LoadDBC<SpellEntry>();
+        public static DB2Storage<SpellMiscEntry> SpellMisc                               { get; private set; } = LoadDB2<SpellMiscEntry>();
+        public static DBCStorage<SpellEffectEntry> SpellEffect                           { get; private set; } = LoadDBC<SpellEffectEntry>();
+        public static DBCStorage<SpellTargetRestrictionsEntry> SpellTargetRestrictions   { get; private set; } = LoadDBC<SpellTargetRestrictionsEntry>();
+        public static DB2Storage<SpellAuraRestrictionsEntry> SpellAuraRestrictions       { get; private set; } = LoadDB2<SpellAuraRestrictionsEntry>();
+        public static DBCStorage<SpellCooldownsEntry> SpellCooldowns                     { get; private set; } = LoadDBC<SpellCooldownsEntry>();
+        public static DBCStorage<SpellCategoriesEntry> SpellCategories                   { get; private set; } = LoadDBC<SpellCategoriesEntry>();
+        public static DBCStorage<SpellShapeshiftEntry> SpellShapeshift                   { get; private set; } = LoadDBC<SpellShapeshiftEntry>();
+        public static DBCStorage<SpellAuraOptionsEntry> SpellAuraOptions                 { get; private set; } = LoadDBC<SpellAuraOptionsEntry>();
+        public static DBCStorage<SpellLevelsEntry> SpellLevels                           { get; private set; } = LoadDBC<SpellLevelsEntry>();
+        public static DB2Storage<SpellClassOptionsEntry> SpellClassOptions               { get; private set; } = LoadDB2<SpellClassOptionsEntry>();
+        public static DB2Storage<SpellCastingRequirementsEntry> SpellCastingRequirements { get; private set; } = LoadDB2<SpellCastingRequirementsEntry>();
+        public static DB2Storage<SpellPowerEntry> SpellPower                             { get; private set; } = LoadDB2<SpellPowerEntry>();
+        public static DBCStorage<SpellInterruptsEntry> SpellInterrupts                   { get; private set; } = LoadDBC<SpellInterruptsEntry>();
+        //public static DBCStorage<SpellEquippedItemsEntry>       SpellEquippedItems     { get; private set; } = LoadDBC<SpellEquippedItemsEntry>();
+        public static DB2Storage<SpellRadiusEntry> SpellRadius                           { get; private set; } = LoadDB2<SpellRadiusEntry>();
+        public static DB2Storage<SpellCastTimesEntry> SpellCastTimes                     { get; private set; } = LoadDB2<SpellCastTimesEntry>();
+        public static DB2Storage<SpellMiscDifficultyEntry> SpellDifficulty               { get; private set; } = LoadDB2<SpellMiscDifficultyEntry>();
+        public static DB2Storage<SpellRangeEntry> SpellRange                             { get; private set; } = LoadDB2<SpellRangeEntry>();
+        public static DB2Storage<SpellReagentsEntry> SpellReagents                       { get; private set; } = LoadDB2<SpellReagentsEntry>();
+        public static DB2Storage<SpellDurationEntry> SpellDuration                       { get; private set; } = LoadDB2<SpellDurationEntry>();
+        public static DBCStorage<SkillLineAbilityEntry> SkillLineAbility                 { get; private set; } = LoadDBC<SkillLineAbilityEntry>();
+        public static DBCStorage<SkillLineEntry> SkillLine                               { get; private set; } = LoadDBC<SkillLineEntry>();
+        //public static DBCStorage<ScreenEffectEntry>             ScreenEffect           { get; private set; } = LoadDBC<ScreenEffectEntry>();
+        //public static DBCStorage<OverrideSpellDataEntry>        OverrideSpellData      { get; private set; } = LoadDBC<OverrideSpellDataEntry>();
+        //public static DBCStorage<ItemClassEntry>                ItemClass              { get; private set; } = LoadDBC<ItemClassEntry>();
+        public static DBCStorage<SpellShapeshiftFormEntry> SpellShapeshiftForm           { get; private set; } = LoadDBC<SpellShapeshiftFormEntry>();
+        public static DB2Storage<SpellDispelTypeEntry> SpellDispelType                   { get; private set; } = LoadDB2<SpellDispelTypeEntry>();
+        public static DB2Storage<ItemSparseEntry> ItemSparse                             { get; private set; } = LoadDB2<ItemSparseEntry>("Item-sparse.db2");
+
+        public static void Load() { }
+
+        static DBCStorage<T> LoadDBC<T>(string file = "") where T : class, new()
+        {
+            if (string.IsNullOrWhiteSpace(file))
+            {
+                file = typeof(T).Name;
+                file = file.Substring(0, file.Length - "Entry".Length) + ".dbc";
+            }
+            var fileName = Path.Combine(DBC_PATH, file);
+
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException($"File '{fileName}' not found", fileName);
+
+            var bytes = File.ReadAllBytes(fileName);
+            using (var ms = new MemoryStream(bytes))
+            {
+                try
+                {
+                    var tmp = new DBCStorage<T>();
+                    tmp.Load(ms);
+                    return tmp;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Can't load " + fileName, ex);
+                }
+            }
+        }
+
+        static DB2Storage<T> LoadDB2<T>(string file  = "") where T : class, new()
+        {
+            if (string.IsNullOrWhiteSpace(file))
+            {
+                file = typeof(T).Name;
+                file = file.Substring(0, file.Length - "Entry".Length) + ".db2";
+            }
+            var fileName = Path.Combine(DBC_PATH, file);
+
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException($"File '{fileName}' not found", fileName);
+
+            var bytes = File.ReadAllBytes(fileName);
+            using (var ms = new MemoryStream(bytes))
+            {
+                try
+                {
+                    var tmp = new DB2Storage<T>();
+                    tmp.Load(ms);
+                    return tmp;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Can't load " + fileName, ex);
+                }
+            }
+        }
     }
 }
